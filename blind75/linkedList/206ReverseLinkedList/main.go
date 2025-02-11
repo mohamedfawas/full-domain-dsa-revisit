6540b4768354c24fe7acddcd75dac6514e75f43d
mohamedfawas <fawastmh@gmail.com>
2025-02-12 04:52:37 +0530

    206. Reverse Linked List

---
+++ blind75/linkedList/206ReverseLinkedList/main.go
@@ -0,0 +1,26 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	newHead := reverseList(head.Next)
+	front := head.Next
+	front.Next = head
+	head.Next = nil
+
+	return newHead
+}
